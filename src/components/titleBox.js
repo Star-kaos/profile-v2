eb0f3b100cd99233541e9f226e369b8ea63a6a79
elijah <perera.elijah@gmail.com>
2022-03-24 12:40:41 -0700

    test

--- src/components/titleBox.js
+++ src/components/titleBox.js
@@ -7,7 +7,7 @@ function titleBox() {
             <div className="titleBox">
                 <div className="titleBoxNest">
                     <div className="titleButton">
-                        <h1 className="titleHeading">Hello, my name is Elijah. Would you like to get in contact?</h1>
+                        <h1 className="titleHeading">Hello, my name is Elijah Perera. Would you like to get in contact?</h1>
                         <div className='contactBtnANest'><a className="contactBtnA" href="#contact"><button className="contactBtn">Contact</button></a></div>
                     </div>
                     <div className="mePicNest">
