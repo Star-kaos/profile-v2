bdd819713f657d6887a01be1b569d42c6d62bb7a
Elijah Perera <36169107+Star-kaos@users.noreply.github.com>
2022-03-31 08:31:45 -0700

    Completed subheading for achievements

--- src/components/achievments.js
+++ src/components/achievments.js
@@ -5,7 +5,10 @@ function achievments() {
 
         <div className="achievmentBoxMain">
             <div className="achievmentBoxMainNest">
-                <h3 className='achievmentsH1'>HTML & CSS responsive web design certificate.</h3>
+                <div className="achievmentBoxHeadings">
+                    <h3 className='achievmentsH1'>HTML & CSS responsive web design certificate.</h3>
+                    <h3 className='achievmentsH1'>This is my html and css responsive web design certificate. This is a reward for completing three-hundred hours worth of work. This course contained CSS, HTML, Flexbox.</h3>
+                </div>
                 <div className='achievmentsImg'></div>
             </div>
         </div>
