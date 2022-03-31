d69592a46dd39a7ba9367e6200804234f14474fb
Elijah Perera <36169107+Star-kaos@users.noreply.github.com>
2022-03-31 08:32:44 -0700

    Fixed className issue

--- src/components/achievments.js
+++ src/components/achievments.js
@@ -7,7 +7,7 @@ function achievments() {
             <div className="achievmentBoxMainNest">
                 <div className="achievmentBoxHeadings">
                     <h3 className='achievmentsH1'>HTML & CSS responsive web design certificate.</h3>
-                    <h3 className='achievmentsH1'>This is my html and css responsive web design certificate. This is a reward for completing three-hundred hours worth of work. This course contained CSS, HTML, Flexbox.</h3>
+                    <p className='achievmentsP'>This is my html and css responsive web design certificate. This is a reward for completing three-hundred hours worth of work. This course contained CSS, HTML, Flexbox.</p>
                 </div>
                 <div className='achievmentsImg'></div>
             </div>
