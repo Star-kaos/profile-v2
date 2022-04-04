b661734f934899efa7374a27035f455755eefb42
Star-kaos <perera.elijah@gmail.com>
2022-04-03 21:05:46 -0700

    fixed grammer in ptags

--- src/components/achievments.js
+++ src/components/achievments.js
@@ -6,7 +6,7 @@ function achievments() {
         <div className="achievmentBoxMain">
             <div className="achievmentBoxMainNest">
                 <div className="achievmentBoxHeadings">
-                    <h3 className='achievmentsH1'>HTML & CSS responsive web design certificate.</h3>
+                    <h3 className='achievmentsH1'>HTML & CSS responsive web design certificatation.</h3>
                     <p className='achievmentsP'>This is my html and css responsive web design certificate. This is a reward for completing three-hundred hours worth of work. This course contained CSS, HTML, Flexbox.</p>
                 </div>
                 <div className='achievmentsImg'></div>
